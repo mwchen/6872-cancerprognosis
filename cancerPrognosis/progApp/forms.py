53962d09017177659b75c8af2dcd87b91ce36580
mwchen <mwchen@mit.edu>
2014-11-16 20:30:39 -0500

    add a nav bar to the pages. Need to get rid of the extra spacing still.

--- cancerPrognosis/progApp/forms.py
+++ cancerPrognosis/progApp/forms.py
@@ -1,17 +1,21 @@
 from django import forms
 from django.db import models
 from django.forms import ModelChoiceField
-from progApp.models import CancerData
+from progApp.models import Cancer, CancerData, Stage, Treatment
 
 # This model will be used for keeping track of the type of cancer.
 class LookUpForm(forms.ModelForm):
-	#cancer = forms.ForeignKey(Cancer, help_text="Please enter the type of cancer: ")
-	#treatment = forms.ForeignKey(Treatment, null = True, blank = True)
-	#stage = forms.ForeignKey(Stage, null = True, blank = True)
+	# TODO: Need to fix the message to get rid of 
+	# "Hold down "Control", or "Command" on a Mac, to select more than one. "
+	#cancer = forms.ModelMultipleChoiceField(queryset=Cancer.objects.all(), \
+	#	help_text="Type of cancer: ")
+	#stage = forms.ModelMultipleChoiceField(queryset=Stage.objects.all(), \
+	#	help_text="Stage: ")
+
 
 	# 0 none 1 boy 2 girl
-	gender = forms.IntegerField(initial = 0, help_text="Gender: ")
-	age = forms.IntegerField(initial = -1, help_text="Age: ")
+	gender = forms.IntegerField(initial = 0)
+	age = forms.IntegerField(initial = -1)
 
 	# This will depend on the model needed for the database that has not been created yet.
 	class Meta:
