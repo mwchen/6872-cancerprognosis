92ae42c8c04ff89cbfbfd8b02eb6c2553dbccd54
mwchen <mwchen@mit.edu>
2014-11-29 14:08:02 -0500

    modified the update patient page to follow the same style as the rest of hte app

--- cancerPrognosis/progApp/urls.py
+++ cancerPrognosis/progApp/urls.py
@@ -6,7 +6,7 @@ urlpatterns = patterns('progApp.views',
     # url(r'^$', 'cancerPrognosis.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
     url(r'^lookup/$', 'lookup', name='lookup'),
-    url(r'^updatePatient/$', 'updatePatient', name='updatePatient'),
+    url(r'^lookup/updatePatient/$', 'updatePatient', name='updatePatient'),
     url(r'^$', 'index', name='index'),
     url(r'^lookup/getCancerProg' ,views.getCancerProg, name = 'getCancerProg'),
 )
